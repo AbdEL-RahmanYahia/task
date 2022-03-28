dc4b9e0c9e16e5239f292f4582305bf2191e580d
abdo3344 <92889596+abdo3344@users.noreply.github.com>
2022-03-28 21:15:37 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+
+
+$names=["abdo","Yahia","Mahmoud","Hamada","Ahmed","Kareem","Malik","Nour"];
+
+if(in_array("AbdEL_Rahman",$names) OR in_array("abdo",$names)){
+    echo "AbdEL_Rahman already exists ";
+
+}elseif(in_array("Yahia",$names) OR in_array("yaya",$names)){
+    echo "Yahia already exists ";
+
+}elseif(in_array("Mahmoud",$names) OR in_array("hoda",$names)){
+    echo "Mahmoud already exists ";
+
+}elseif(in_array("Hamada",$names) OR in_array("medo",$names)){
+    echo "Hamada already exists ";
+
+}elseif(in_array("Ahmed",$names) OR in_array("Hamada",$names)){
+    echo "Ahmed already exists ";
+
+}elseif(in_array("Kareem",$names) OR in_array("kemo",$names)){
+    echo "Kareem already exists ";
+
+}elseif(in_array("Malik",$names) OR in_array("maloka",$names)){
+    echo "Malik already exists ";
+
+}elseif(in_array("Nour",$names) OR in_array("narnor",$names)){
+    echo "Nour already exists ";
+
+}
